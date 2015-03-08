0f529d3aaf9c33ecdc10f18786d013a072494cc6
msubedee <subedee.mukesh@gmail.com>
2015-03-08 14:25:57 -0500

    made changes in plot1.R

--- plot1.R
+++ plot1.R
@@ -8,5 +8,4 @@ source("read_data.R") # call the script to read data for plotting
 # constructing the histogram and saving it to a PNG file 
 png("plot1.png", height = 480, width = 480)
 hist(subset_PowerCon$Global_active_power, main = "Global Active Power", col = "Red", xlab="Global Active Power (kilowatts)", ylab = "Frequency")
-#dev.copy(png, file="plot1.png", height = 480, width = 480)
 dev.off() # close the PNG device
